4286c92cb77027133c09592c353b9cd9ab33162a
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-17 16:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 //complete this code
-class Animal {}
+class Animal {
+	Animal(species){
+		this.species = species;
+		get species(){
+			return this.species;
+		}
+	}
+	makeSound(sound){
+		console.log(sound);
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	let bark() = makeSound("woof");
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	let purr() = makeSound("purr");
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
