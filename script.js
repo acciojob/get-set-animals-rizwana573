cc2b73b32ff9852c6554c5ad8778ac83c4027050
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-17 17:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-//complete this code
 class Animal {
-	Animal(species){
-		this.species = species;
-		get species(){
-			return this.species;
-		}
-	}
-	makeSound(sound){
-		console.log(sound);
-	}
-}
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {
-	let bark() = makeSound("woof");
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
+  }
 }
 
 class Cat extends Animal {
-	let purr() = makeSound("purr");
+  purr() {
+    console.log("purr");
+  }
 }
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+const animal = new Animal("Unknown");
+console.log(animal.species); 
+animal.makeSound(); 
+
+const cat = new Cat("Cat");
+console.log(cat.species); 
+cat.makeSound(); 
+cat.purr();
+
+const dog = new Dog("Dog");
+console.log(dog.species); 
+dog.makeSound(); 
+dog.bark(); 
 
 // Do not change the code below this line
 window.Animal = Animal;
